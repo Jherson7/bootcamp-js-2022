1f7ed1a1b18e717fb9cadce7acdaf169b7a435ba
Jherson Sazo <Jherson.sazo@galileo.edu>
2022-02-24 20:16:34 -0600

    Ejemplo inicial

---
+++ index.js
@@ -0,0 +1,7 @@
+let paragraph = document.getElementsByTagName("p");
+
+if (paragraph.length>0){
+    let p_1 = paragraph[0];
+    p_1.innerHTML ="Bienvenidos a todos jeje"
+    console.log(paragraph)
+}
